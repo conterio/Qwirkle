57657a5c91d05fde17d15f75c7038e57c56d0e8b
Chad Parmeter <cdparmeter@xactware.com>
2018-11-09 15:07:28 -0700

    added simple busi interfaces and new turn response model stub

---
+++ Busi/IBusi/IGameBusi.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Busi.IBusi
+{
+	public interface IGameBusi
+	{
+		Guid CreateGame();
+		bool JoinGame(Guid gameId);
+		bool JoinGameAsObserver(Guid gameid);
+		Dictionary<Player, List<Tile>> StartGame(Guid gameId);
+		PlayTurnResponse PlayMove(Guid GameId, ITurn move, Player player);
+	}
+}
---
+++ Busi/IBusi/IGameManager.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Busi.IBusi
+{
+	public interface IGameManager
+	{
+		Guid CreateGame();
+		bool IsMoveValid(ITurn move);
+		List<List<Tile>> ApplyMove(ITurn move);
+	}
+}
