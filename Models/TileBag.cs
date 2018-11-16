bd524cdba2f2d505742f7be608ddbdcc6f8403cc
Russell <ramcdonnell@xactware.com>
2018-11-16 14:01:48 -0700

    Updated TileBag constructor

--- Models/TileBag.cs
+++ Models/TileBag.cs
@@ -11,60 +11,37 @@ namespace Models
             Tiles = new List<Tile>();
 
             //Fill bag with tiles
-            for (int i = 0; i < numOfSameShapeAndColor; ++i)
-            {
-                foreach (Color color in Enum.GetValues(typeof(Color)))
-                {
-                    foreach (Shape shape in Enum.GetValues(typeof(Shape)))
-                    {
-                        Tiles.Add(new Tile(color, shape));
-                    }
-                }
-            }
+            Tiles = (from _ in Enumerable.Repeat(0, numOfSameShapeAndColor)
+                     from color in Enum.GetValues(typeof(Color)).Cast<Color>()
+                     from shape in Enum.GetValues(typeof(Shape)).Cast<Shape>()
+                     select new Tile(color, shape)).ToList();
 
             Shuffle();
         }
+
         private List<Tile> Tiles;
 
         public List<Tile> DrawTiles(int numberOfTiles)
         {
-            var drawnTiles = new List<Tile>();
-            for(int i = 0; i < numberOfTiles; ++i)
-            {
-                if(Tiles.Any())
-                {
-                    drawnTiles.Add(Tiles[0]);
-                    Tiles.RemoveAt(0);
-                }
-            }
+            var drawnTiles = Tiles.Take(numberOfTiles).ToList();
+            Tiles.RemoveRange(0, drawnTiles.Count);
             return drawnTiles;
         }
 
-        public List<Tile> ReturnTiles(List<Tile> tiles)
+        public List<Tile> ReturnTiles(List<Tile> returnedTiles)
         {
             //Check if there are enough tiles in the bag
-            if(tiles.Count() > Tiles.Count())
+            if (returnedTiles.Count > Tiles.Count)
             {
                 //There aren't enough tiles this is an invalid move
                 throw new Exception("There aren't enough tiles");
             }
 
             //Set aside the tiles you want to trade and draw replacement tiles
-            var drawnTiles = new List<Tile>();
-            for (int i = 0; i < tiles.Count(); ++i)
-            {
-                if (Tiles.Any())
-                {
-                    drawnTiles.Add(Tiles[0]);
-                    Tiles.RemoveAt(0);
-                }
-            }
+            var drawnTiles = DrawTiles(returnedTiles.Count);
 
             //mix the tiles that you traded away back into the bag
-            foreach (var tile in tiles)
-            {
-                Tiles.Add(tile);
-            }
+            Tiles.AddRange(returnedTiles);
             Shuffle();
 
             return drawnTiles;
