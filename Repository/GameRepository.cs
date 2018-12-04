d9dfca153884e7b72d4db39d7b5e1279c47c7366
Russell <ramcdonnell@xactware.com>
2018-12-04 16:11:04 -0700

    IOC, implemented IOC framework

---
+++ Repository/GameRepository.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+
+namespace Repository
+{
+    public interface IGameRepository
+    {
+        Game GetGame(Guid guid);
+    }
+
+    public class GameRepository : IGameRepository
+    {
+        public Game GetGame(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
